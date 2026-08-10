b94d47e672f7afc36ff0fcbe2be180f1a74517c9
pawan kushwah <pawankushwah23713@gmail.com>
2026-08-10 23:46:58 +0530

    remove this route

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import  AppRouters from './Routes.jsx';
+
 import LaundryMachineApp from './pages/Laundrymachineapp.jsx';
 
 function App() {
